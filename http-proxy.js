7d783191b206bc81321efbe112aa1fc4ca0a7845
Peter Reid <peter.d.reid@gmail.com>
2012-10-12 23:47:02 -0400

    Test is now standalone
    Before, I would run the proxy server and then run the test against it. Now
    the test starts up the proxy server and an HTTP server for it to forward
    to.
    
    I also renamed index.js to http-proxy.js

--- http-proxy.js
+++ http-proxy.js
@@ -1,12 +1,4 @@
-var tls = require('tls');
-var fs = require('fs');
-var net = require('net');
 var HttpParser = require('./http-parser');
-
-var options = { // HTTPS options
-  key: fs.readFileSync(process.argv[2]),
-  cert: fs.readFileSync(process.argv[3])
-};
 
 // When we get lots of data before we know what the destination host is, we have to
 // buffer it. Limiting that prevents malformed data from filling up our memory.
@@ -14,41 +6,6 @@ var options = { // HTTPS options
 // if that packet puts us over that limit.
 // In practice, this is roughly how long a URI is allowed.
 var PREHOST_MAX_LENGTH = 5*1024;
-
-//todo: overwrite write()
-/*
-function validateTargets(ts) {
-  if (typeof ts !== 'object') {
-    throw new Error('Targets must be an object mapping host to {host, port}');
-  }
-  for (var target in ts) {
-    if (typeof target.host !== 'string') {
-      throw new Error('Target ' + target + ' does not have a valid host.');
-    } else if (typeof target.port !== 'number' || target.port != (target.port|0)) {
-      throw new Error('Target ' + target + ' does not have a valid port.');
-    }
-  }
-  return ts;
-}
-
-var targets = {};
-var readTargetsFile = function(cb) {
-  return fs.readFile('./targets.json', function(err, json) {
-    if (err) {
-        console.error('targets.json parse failed:', e);
-        cb(e);
-    }
-
-    try {
-      targets = validateTargets(JSON.parse(json)));
-      cb(null);
-    } catch (e) {
-      console.error('targets.json parse failed:', e);
-      cb(e);
-    }
-  });
-}
-*/
 
 /* Throttled console.error, since it is blocking */
 var reportError = (function() {
@@ -76,38 +33,6 @@ var reportError = (function() {
   }
 })();
 
-var targets = {
-  'localproxy.flightvector.com': {
-    host: '127.0.0.1',
-    port: 8082
-  },
-  'weather.flightvector.com': {
-    host: '127.0.0.1',
-    port: 3001
-  },
-  'proxy1.flightvector.com': {
-    host: '127.0.0.1',
-    port: 8082
-  },
-  'map.flightvector.com': {
-    host: '127.0.0.1',
-    port: 8083
-  },
-  '127.0.0.2': {
-    host: '127.0.0.1',
-    port: 8082
-  },
-  '127.0.0.3': {
-    host: '127.0.0.1',
-    port: 8083
-  }
-};
-
-function onConnection(stream) {
-  stream.httpParser = new HttpParser();
-
-  stream.on('data', onPrehostData);
-}
 
 function httpResponse(code, message) {
   return 'HTTP/1.1 ' + code + ' ' + message + '\r\n\r\n' + message;
@@ -122,19 +47,19 @@ function onPrehostData(buffer) {
   this.httpParser.advance(buffer);
   
   if (this.httpParser.done()) {
-    var target = targets[this.httpParser.host];
-
-    if (!target) {
+    var targetStream = this._targetCb.call(this,
+      this.httpParser.host, this.httpParser.uri);
+    
+    if (!targetStream) {
       reportError('Invalid target: ' + this.httpParser.host);
 
       requestorStream.end(HOST_NOT_FOUND_ERROR);
       return;
     }
 
-    var targetStream = net.connect(target.port, target.host);
     var gotSomeFromTarget = false;
     targetStream.on('error', function(err) {
-      reportError('Target stream to ' + target.host + ':' + target.port + ' failed.');
+      reportError('Target stream for ' + this.httpParser.host + this.httpParser.uri + ' failed.');
       requestorStream.end(gotSomeFromTarget ? '' : TARGET_FAILED);
     });
     targetStream.once('data', function() {
@@ -159,6 +84,18 @@ function onPrehostData(buffer) {
   }
 }
 
-var server = tls.createServer(options);
-server.on('secureConnection', onConnection);
-server.listen(443);
+/*
+ * Proxy one HTTP stream into another.
+ * 
+ * stream :: Stream
+ *   readable stream that will send HTTP
+ * targetCb :: (host, uri) -> stream?
+ *   Provide an output stream for the given request. May return null.
+ *   'this' is set to the the input stream
+ */
+var proxyStream = module.exports.proxyStream = function(stream, targetCb) {
+  stream.httpParser = new HttpParser();
+  stream._targetCb = targetCb;
+  stream.on('data', onPrehostData);
+}
+
--- test.js
+++ test.js
@@ -1,8 +1,46 @@
 var https = require('https');
+var http = require('http');
 var assert = require('assert');
+var tls = require('tls');
+var fs = require('fs');
+var net = require('net');
+var proxyStream = require('./http-proxy').proxyStream;
+
+var targetServer = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(18082);
+
+var targets = {
+  '127.0.0.2': {
+    host: '127.0.0.1',
+    port: 18082
+  },
+  '127.0.0.3': {
+    host: '127.0.0.1',
+    port: 18083
+  }
+};
+
+var proxyServer = tls.createServer({ // HTTPS options
+  key: fs.readFileSync('./dummy.key'),
+  cert: fs.readFileSync('./dummy.crt')
+});
+proxyServer.on('secureConnection', function(stream) {
+  proxyStream(stream, function(host, uri) {
+    var target = targets[host];
+    if (!target) return null;
+    
+    return net.connect(target.port, target.host);
+  });
+});
+proxyServer.listen(443);
+
+
+var testComplete = false;
 
 var options = {
-  host: 'localproxy.flightvector.com',
+  host: '127.0.0.2',
   port: 443,
   path: '/connect',
   method: 'GET'
@@ -14,9 +52,15 @@ var req = https.get(options, function(res) {
   res.on('data', function(d) {
     process.stdout.write(d);
   });
+  res.on('end', function() {
+    testComplete = true;
+    proxyServer.close();
+    targetServer.close();
+  });
 }).on('error', function(e) {
   console.error(e);
-});
+})
+req.end();
 
 var sockets = req.agent.sockets[options.host + ':' + options.port];
 assert(sockets.length === 1);
